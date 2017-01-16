2e9c3b6496161f9300e09e136e917169f93391c2
pealpool P <pealpool@gmail.com>
2017-01-16 23:37:47 +0800

    222

--- DownloaderTime HHF/Form1.Designer.vb
+++ DownloaderTime HHF/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.TrackBar1 = New System.Windows.Forms.TrackBar()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
@@ -176,6 +177,7 @@ Partial Class Form1
         Me.Controls.Add(Me.TextBox_Volume)
         Me.Controls.Add(Me.Label_Out)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
